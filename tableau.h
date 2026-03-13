7785e12691784875acbfa4089ea38d0123cf6d1d
PSYAMEN <nicolas.2.brebisson@gmail.com>
2026-03-13 16:04:20 +0100

    indication supp

--- tableau.h
+++ tableau.h
@@ -21,6 +21,7 @@ private:
     Shape* availShape[12];
     SavePlacement placedShapes[12];//pas un pointeur come on prend une copie parce que si c'est des pointeur on peu pas le comparer a ceux dans availShapes
     int nbPlacedShapes;//le nombre de formes placer(fini le prog si )
+    //donne aussi en qq sorte la prochiane shape a placer
 public:
     Tableau();//tableau de 5x12 avec toute les forme de disponible
     Tableau(int nbL);//shuffel et met de maniere aleatoir les shape dans availShapes pour pas toujour avoir les meme si le tableau est pas entier (comme chaque shape fait 5 case il y a nbL shape dans le tableau de disponible)
@@ -29,7 +30,7 @@ public:
     bool canPlace(int indiceS,int x,int y);//on prend une forme et on verifie que elle ets pas sur une autre en fonctione de x et y(sa position) faudra aussi faire gaffe a pas faire des check en dehors du tableau!!! sinon seg fault (tu fait just in if(x+shape.cell[i]>0 && x+shape.cell[i]<12))
     int nbOpti(int indiceS,int x,int y);//on renvoie le nombre de case sur laquel elle est a coter (diagonale compris normalement)
     void placeShape(int indiceS,int x,int y);//on pose la forme du tableau on recolaps aussi le tableau de shape deriere (on utilisera toujour la shapes[0] comme prochain placement )
-    void removeShape(int i);//on enleve le ieme element de placed shape(generalement le darnier) et on desincrement le nextShape
+    void removeShape(int i);//on enleve le ieme element de placed shape(generalement le darnier) et on desincrement le nbPlacedShape
     void algorythmeDePlacage();//on recupere le position optimale de chaque forme et le nombre de cas e elle on a coter puis on place la plus optimal(rotation inclus)(celle qui es a coter du plus grand nombre de cases) jusqua que l on puisse plus puis si le tableau pas rempli on nelve la derniere placer et on reregarde pour elle si elle peut etre placer differament et ainsi de suite en gros le main (il appel aussi render)
-
+    //la condition de fin est qqch du genre nbPlacedShape!=nbLigne
 };
